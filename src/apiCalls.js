101119097e4ca9f122b33f3bc6023b6ffa59d439
Prissilla Escobar <prissilla988@gmail.com>
2023-11-08 23:53:02 -0700

    Changed album and song fetch request to async await functions

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,17 +1,4 @@
-// export function getAlbums() {
-//     return fetch("https://taylor-swift-api.sarbo.workers.dev/albums").then(
-//       (response) => {
-//         if (!response.ok) {
-//           throw new Error(`Album not found.`);
-//         }
-//         return response.json();
-//       }
-//     );
-//   }
-
-// Is async necessary? Otherwise the .map() randomly does not work
-
-  export async function getAlbums() {
+export async function getAlbums() {
     let Url = 'https://taylor-swift-api.sarbo.workers.dev/albums'
     try {
         const response = await fetch(Url);
@@ -24,35 +11,26 @@
     }
 }
 
-export function getAlbumSongs(albumId) {
-return fetch(`https://taylor-swift-api.sarbo.workers.dev/albums/${albumId}`).then(
-    (response) => {
-    if (!response.ok) {
-        throw new Error(`Album songs not found.`);
+export async function getAllSongs() {
+    let Url = 'https://taylor-swift-api.sarbo.workers.dev/songs'
+    try {
+        const response = await fetch(Url);
+        if (!response.ok) {
+            throw new Error(`Songs not found.`)
+        }
+        return response.json()
+    } catch (error) {
+        throw new Error(`Error fetching albums: ${error.message}`)
     }
-    return response.json();
-    }
-);
-}
-
-export function getAllSongs() {
-return fetch("https://taylor-swift-api.sarbo.workers.dev/songs").then(
-    (response) => {
-    if (!response.ok) {
-        throw new Error(`Song(s) not found.`);
-    }
-    return response.json();
-    }
-);
 }
 
 export function getSongLyrics(songID) {
-return fetch(`https://taylor-swift-api.sarbo.workers.dev/lyrics/${songID}`).then(
-    (response) => {
-    if (!response.ok) {
-        throw new Error(`Song(s) not found.`);
-    }
-    return response.json();
-    }
-);
+    return fetch(`https://taylor-swift-api.sarbo.workers.dev/lyrics/${songID}`).then(
+        (response) => {
+        if (!response.ok) {
+            throw new Error(`Song(s) not found.`);
+        }
+        return response.json();
+        }
+    )
 }
