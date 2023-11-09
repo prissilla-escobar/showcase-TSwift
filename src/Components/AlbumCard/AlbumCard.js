277a051acd0d887d239163e308aec5be3ae45326
Prissilla Escobar <prissilla988@gmail.com>
2023-11-08 23:51:58 -0700

    Add useEffect for getting all songs. Add functionality to search for songs by album id and display in the return of the card. Will still need to add dropdown feature

--- src/Components/AlbumCard/AlbumCard.js
+++ src/Components/AlbumCard/AlbumCard.js
@@ -2,12 +2,31 @@ import './AlbumCard.css'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 import { albumCovers } from '../../albumCovers'
+import dropdown from '../../Assets/download.png'
+import {useState, useEffect} from 'react'
+import { getAllSongs } from '../../apiCalls'
 
-function AlbumCard( { album_id, title } ) {
-
+function AlbumCard( { album_id, title, setServerError } ) {
+    const [songs, setSongs] = useState([])
     const singleAlbum = albumCovers.find(album => {
         if (album.id === album_id) {
             return album
+        }
+    })
+
+    useEffect(() => {
+        getAllSongs()
+        .then(data => {
+            setSongs(data)
+        })
+        .catch((error) => {
+            setServerError({hasError: true, message: `${error.message}`});
+          })
+    }, [])
+
+    const albumSongsList = songs.map((song) => {
+        if (song.album_id === album_id) {
+            return <div className='song-list'> {song.title} </div>
         }
     })
 
@@ -15,7 +34,9 @@ function AlbumCard( { album_id, title } ) {
         <div className='album-card'>
             <img className='album-image' classID={`album-${title}`}  alt={`${title} album cover`} src={`${singleAlbum.image}`} />
             <div className='album-title'>
-                <h2>{title}</h2>
+                <h2 className='album-title-font'>{title}</h2>
+                <img className='dropimg' src={dropdown} />
+                <div> {albumSongsList} </div>
             </div>
         </div>
     )
