3de258d0127d48d0e82da462de2fc4078bfbfece
Prissilla Escobar <prissilla988@gmail.com>
2023-11-09 12:49:05 -0700

    Change className of albumSongsList return so that each song can have a hover feature. Create handleDropdown function to be called onClick when the open/close images are clicked on. Create state for showSongs in order to toggle. Conditionally render the open or close image for the songs drop down based on if showSongs is true or false

--- src/Components/AlbumCard/AlbumCard.js
+++ src/Components/AlbumCard/AlbumCard.js
@@ -3,11 +3,14 @@ import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 import { albumCovers } from '../../albumCovers'
 import dropdown from '../../Assets/download.png'
+import closeList from '../../Assets/upload.png'
 import {useState, useEffect} from 'react'
 import { getAllSongs } from '../../apiCalls'
 
+
 function AlbumCard( { album_id, title, setServerError } ) {
     const [songs, setSongs] = useState([])
+    const [showSongs, setShowSongs] = useState(false)
     const singleAlbum = albumCovers.find(album => {
         if (album.id === album_id) {
             return album
@@ -26,17 +29,37 @@ function AlbumCard( { album_id, title, setServerError } ) {
 
     const albumSongsList = songs.map((song) => {
         if (song.album_id === album_id) {
-            return <div className='song-list'> {song.title} </div>
+            return <div className='songs'> {song.title} </div>
         }
     })
+
+    const handleDropdown = () => {
+        setShowSongs(!showSongs)
+    }
 
     return (
         <div className='album-card'>
             <img className='album-image' classID={`album-${title}`}  alt={`${title} album cover`} src={`${singleAlbum.image}`} />
             <div className='album-title'>
                 <h2 className='album-title-font'>{title}</h2>
-                <img className='dropimg' src={dropdown} />
-                <div> {albumSongsList} </div>
+                {showSongs ? (
+                <div className='album-title'>
+                    <img
+                        className='openCloseImg'
+                        src={closeList}
+                        onClick={handleDropdown}
+                    />
+                    <div className='song-list'>
+                        {albumSongsList}
+                    </div>
+                </div>
+            ) : (
+                <img
+                    className='openCloseImg'
+                    src={dropdown}
+                    onClick={handleDropdown}
+                />
+            )}
             </div>
         </div>
     )
