9272f7488942a2fc564d70defea1ee7e09312d5a
Prissilla Escobar <prissilla988@gmail.com>
2023-11-10 18:44:15 -0700

    Import Header component, create state for all Lyrics and searchText, import getAllSongs function and create useEffect for it. Render Header component in the return and pass through setSearchText. Pass searchText and allLyrics into SelectedSongLyrics

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import './App.css'
 import { useState, useEffect } from 'react'
-import { getAlbums } from './apiCalls'
+import { getAlbums, getAllSongLyrics } from './apiCalls'
 import AlbumContainer from './Components/AlbumContainer/AlbumContainer'
 import { Route, Routes, useLocation } from 'react-router-dom'
 import SelectedSongLyrics from './Components/SelectedSongLyrics/SelectedSongLyrics'
 import ServerError from './Components/ServerError/ServerError'
 import LoadingComponent from './Components/LoadingComponent/LoadingComponent'
+import Header from './Components/Header/Header'
 
 function App() {
   const [serverError, setServerError] = useState({hasError: false, message: ''})
   const [albums, setAlbums] = useState([])
   const [isLoading, setIsLoading] = useState(true)
+  const [allLyrics, setAllLyrics] = useState([])
+  const [searchText, setSearchText] = useState('')
   const location = useLocation()
   
 useEffect(() => {
@@ -18,6 +21,16 @@ useEffect(() => {
     .then(data => {
       setAlbums(data)
       setIsLoading(false)
+    })
+    .catch(error => {
+      setServerError({hasError: true, message: `${error.message}`})
+    })
+}, [])
+
+useEffect(() => {
+  getAllSongLyrics()
+    .then(data => {
+      setAllLyrics(data)
     })
     .catch(error => {
       setServerError({hasError: true, message: `${error.message}`})
@@ -38,13 +51,14 @@ const resetError = () => {
 
   return (
     <div className="App">
+      <Header setSearchText={setSearchText} />
       {serverError.hasError ? (
       <ServerError resetError={resetError} serverError={serverError} />
     ) : isLoading ? (
         <LoadingComponent />
     ) : (
       <Routes>
-        <Route path='/' element={<AlbumContainer albums={albums} setServerError={setServerError} setIsLoading={setIsLoading} />} />
+        <Route path='/' element={<AlbumContainer albums={albums} setServerError={setServerError} setIsLoading={setIsLoading} searchText={searchText} allLyrics={allLyrics} />} />
         <Route path='/song/:id' element={<SelectedSongLyrics setServerError={setServerError} setIsLoading={setIsLoading} isLoading={isLoading} />} />
         <Route path='*' element={<ServerError resetError={resetError} serverError={serverError} />} />
       </Routes>
